5f2703f811ca2a55de29746297638052abf87f77
GabrielBR6897 <gabrielbahia443@gmail.com>
2026-05-31 22:01:32 -0300

    Adicionadas dentro do portal do professor as telas de dashboard, registrar frequência, lista de espera e a adição de cursos.

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,12 @@ export default function App() {
           </ProtectedRoute>
         } />
 
+        {/* Área do professor */}
+        <Route path="/professor/dashboard" element={<ProfessorDashboard />} />
+        <Route path="/professor/cursos" element={<GerenciarModulos />} />
+        <Route path="/professor/lista-espera" element={<ListaEspera />} />
+        <Route path="/professor/frequencia" element={<Frequencia />} />
+
         {/* Páginas futuras */}
         <Route path="/meus-cursos"         element={<Navigate to="/inicio" replace />} />
         <Route path="/configuracoes"       element={<Navigate to="/inicio" replace />} />
